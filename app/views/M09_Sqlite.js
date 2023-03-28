b15acbe49eebb85cb46d27872b1481e00cb0814c
davsun22daw <davsun22daw@proton.me>
2023-03-28 12:23:50 +0200

    Ver bonita la tabla

--- app/views/M09_Sqlite.js
+++ app/views/M09_Sqlite.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import * as SQLite from 'expo-sqlite';
-import { ScrollView } from 'react-native'
-import data from './paisos.json';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import * as SQLite from "expo-sqlite";
+import { ScrollView } from "react-native";
+import data from "./paisos.json";
 
 /**
  * Classe que hereta de Component i que treballa amb el
@@ -13,14 +13,53 @@ import data from './paisos.json';
 
 const estils = StyleSheet.create({
   textPeu: {
-    color: 'blue',
-    fontWeight: 'bold',
+    color: "blue",
+    fontWeight: "bold",
     fontSize: 30,
   },
   peu: {
     paddingTop: 10,
     paddingBottom: 10,
-    backgroundColor: '#FA0'
+    backgroundColor: "#FA0",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+  header: {
+    backgroundColor: "#3498db",
+    padding: 20,
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 20,
+    color: "#FFFFFF",
+  },
+  paisContainer: {
+    backgroundColor: "#FFFFFF",
+    borderBottomColor: "#CCCCCC",
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+  paisText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  capitalText: {
+    fontSize: 16,
+    color: "#333333",
+    marginBottom: 5,
+  },
+  poblacionText: {
+    fontSize: 16,
+    color: "#333333",
+    marginBottom: 5,
+  },
+  banderaImagen: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
   },
 });
 
@@ -33,10 +72,8 @@ export class M09_Sqlite extends React.Component {
     };
     db = SQLite.openDatabase("db.db");
 
-    db.transaction(tx => {
-      tx.executeSql(
-        "drop table if exists paisos;"
-      );
+    db.transaction((tx) => {
+      tx.executeSql("drop table if exists paisos;");
       tx.executeSql(
         "create table if not exists paisos (id integer primary key not null, Pais text, Capital text, Poblacion int, Bandera text);"
       );
@@ -46,39 +83,47 @@ export class M09_Sqlite extends React.Component {
   }
 
   insertRecordsFromJson() {
-    db.transaction(tx => {
-      data.forEach(item => {
+    db.transaction((tx) => {
+      data.forEach((item) => {
         const { Pais, Capital, Poblacion, Bandera } = item;
-        tx.executeSql("INSERT INTO paisos (Pais, Capital, Poblacion, Bandera) VALUES (?, ?, ?, ?)", [Pais, Capital, Poblacion, Bandera]);
+        tx.executeSql(
+          "INSERT INTO paisos (Pais, Capital, Poblacion, Bandera) VALUES (?, ?, ?, ?)",
+          [Pais, Capital, Poblacion, Bandera]
+        );
       });
     });
   }
 
   componentDidMount() {
-    db.transaction(tx => {
+    db.transaction((tx) => {
       tx.executeSql("SELECT * FROM paisos", [], (_, { rows }) =>
         this.setState({ paisos: rows._array })
       );
     });
   }
-  
 
   render() {
     const { paisos } = this.state;
 
     return (
-      <View style={estils.peu}>
-        <Text style={estils.textPeu}> SQLITE </Text>
+      <View style={estils.container}>
+        <View style={estils.header}>
+          <Text style={estils.headerText}>PAÍSES DEL MUNDO</Text>
+        </View>
         <ScrollView>
-        {paisos.map(item => (
-          <View key={item.id}>
-            <Text>{item.Pais} - {item.Capital} - {item.Poblacion}</Text>
-            <Image
-            source={{ uri: item.Bandera }}
-            style={{ width: 100, height: 100 }}
-          />
-          </View>
-        ))}
+          {paisos.map((item) => (
+            <View key={item.id} style={estils.paisContainer}>
+              <Text style={estils.paisText}>{item.Pais}</Text>
+              <Text style={estils.capitalText}>Capital: {item.Capital}</Text>
+              <Text style={estils.poblacionText}>
+                Población: {item.Poblacion}
+              </Text>
+              <Image
+                source={{ uri: item.Bandera }}
+                style={estils.banderaImagen}
+              />
+            </View>
+          ))}
         </ScrollView>
       </View>
     );
