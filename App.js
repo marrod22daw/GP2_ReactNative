2e5f419c86eef8a2be65ae7a2bf24656de3277c9
marrod22daw <marrod22daw@proton.me>
2023-03-29 20:16:38 +0200

    Mejor App 2

--- App.js
+++ App.js
@@ -1,27 +1,59 @@
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+/* import { createStackNavigator } from '@react-navigation/stack'; */
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 import {M06_Home} from './app/views/M06_Home_routing';
-import {M06_Detalls} from './app/views/M06_Detalls_routing';
+import {M06_Donacions} from './app/views/M06_Donacions';
 import { M09_Sqlite } from './app/views/M09_Sqlite';
 import { M09_Sqlite_PiB } from './app/views/M09_Sqlite_PiB';
 import { M09_Sqlite_Pobl } from './app/views/M09_Sqlite_Pobl';
 import { M09_Sqlite_EVida } from './app/views/M09_Sqlite_EVida';
 
-const Stack = createStackNavigator();
+/* const Stack = createStackNavigator(); */
+const Tab = createBottomTabNavigator();
+
+const homeNom = "Home";
+const SqliteNom = "SQLite";
+const donacionsNom = "Donacions";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={M06_Home} options={{headerBackTitleVisible: false,headerTitle: '',headerShown: true}}/>
-        <Stack.Screen name="Detall" component={M06_Detalls} options={{headerBackTitleVisible: false,headerTitle: '',headerShown: true}}/>
-        <Stack.Screen name="SQLite" component={M09_Sqlite} options={{headerBackTitleVisible: false,headerTitle: '',headerShown: true}}/>
-        <Stack.Screen name="SQLite_PiB" component={M09_Sqlite_PiB} options={{headerBackTitleVisible: false,headerTitle: '',headerShown: true}}/>
-        <Stack.Screen name="SQLite_Pobl" component={M09_Sqlite_Pobl} options={{headerBackTitleVisible: false,headerTitle: '',headerShown: true}}/>
-        <Stack.Screen name="SQLite_EVida" component={M09_Sqlite_EVida} options={{headerBackTitleVisible: false,headerTitle: '',headerShown: true}}/>
-      </Stack.Navigator>
+      <Tab.Navigator
+       initialRouteName={homeNom}
+       screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let rn = route.name;
+
+          if (rn === homeNom) {
+            iconName = focused ? 'home' : 'home-outline';
+
+          } else if (rn === SqliteNom) {
+            iconName = focused ? 'list' : 'list-outline';
+
+          } else if (rn === donacionsNom) {
+            iconName = focused ? 'settings' : 'credit';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+          
+        },
+        activeTintColor: 'blue',
+        inactiveTintColor: 'grey',
+        labelStyle: { paddingBottom: 10, fontSize: 10 },
+        style: { padding: 10, height: 70}
+      })}>
+        <Tab.Screen name={homeNom} component={M06_Home} options={{headerBackTitleVisible: false,headerTitle: '',headerShown: true}}/>
+        <Tab.Screen name={donacionsNom} component={M06_Donacions} options={{headerBackTitleVisible: false,headerTitle: '',headerShown: true}}/>
+        <Tab.Screen name={SqliteNom} component={M09_Sqlite} options={{headerBackTitleVisible: false,headerTitle: '',headerShown: true}}/>
+        <Tab.Screen name="SQLite_PiB" component={M09_Sqlite_PiB} options={{headerShown: false}}/>
+        <Tab.Screen name="SQLite_Pobl" component={M09_Sqlite_Pobl} options={{headerShown: false}}/>
+        <Tab.Screen name="SQLite_EVida" component={M09_Sqlite_EVida} options={{headerShown: false}}/>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
---
+++ app/views/M06_Donacions.js
@@ -0,0 +1,204 @@
+import React from "react";
+import {
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+const DONATION_OPTIONS = [5, 10, 20];
+
+const estils = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  header: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 32,
+  },
+  headerText: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#333",
+    letterSpacing: 1,
+    textTransform: "uppercase",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "#333",
+    textTransform: "uppercase",
+    textAlign: "center",
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: "#666",
+    textAlign: "center",
+  },
+  input: {
+    height: 50,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    marginBottom: 20,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    width: "100%",
+  },
+  textarea: {
+    height: 150,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    marginBottom: 20,
+    paddingHorizontal: 20,
+    paddingTop: 10,
+    backgroundColor: "#fff",
+    width: "100%",
+    textAlignVertical: "top",
+  },
+  button: {
+    backgroundColor: "red",
+    borderRadius: 4,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    shadowColor: "rgba(0, 0, 0, 0.5)",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.8,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  buttonHover: {
+    opacity: 0.8,
+  },
+  optionContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  optionText: {
+    fontWeight: "bold",
+  },
+  optionButton: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    padding: 10,
+    marginRight: 10,
+  },
+  optionButtonSelected: {
+    backgroundColor: "#007bff",
+    borderColor: "#007bff",
+  },
+  optionButtonText: {
+    color: "#000",
+  },
+  optionButtonTextSelected: {
+    color: "#fff",
+  },
+});
+
+export class M06_Donacions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nom: "",
+      email: "",
+      euro: "",
+      comentari: "",
+      selectedOption: "",
+    };
+  }
+
+  handleOptionSelect = (option) => {
+    this.setState({ selectedOption: option, euro: option.toString() });
+  };
+
+  render() {
+    return (
+      <View style={estils.container}>
+        <View style={estils.header}>
+          <Text style={estils.headerText}>Donacions</Text>
+        </View>
+        <TextInput
+          style={estils.input}
+          placeholder="Nom"
+          onChangeText={(text) => this.setState({ nom: text })}
+          value={this.state.nom}
+        />
+        <TextInput
+          style={estils.input}
+          placeholder="Email"
+          onChangeText={(text) => this.setState({ email: text })}
+          value={this.state.email}
+        />
+        <View style={estils.optionContainer}>
+          <Text style={estils.optionText}>Selecciona una opción: </Text>
+          {DONATION_OPTIONS.map((option) => (
+            <TouchableOpacity
+              key={option}
+              style={[
+                estils.optionButton,
+                this.state.selectedOption === option &&
+                  estils.optionButtonSelected,
+              ]}
+              onPress={() => this.handleOptionSelect(option)}
+            >
+              <Text
+                style={[
+                  estils.optionButtonText,
+                  this.state.selectedOption === option &&
+                    estils.optionButtonTextSelected,
+                ]}
+              >
+                {option}€
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <TextInput
+          style={[estils.input, { marginTop: 10 }]}
+          placeholder="Suma de €"
+          onChangeText={(int) =>
+            this.setState({ euro: int, selectedOption: "" })
+          }
+          value={this.state.euro}
+        />
+        <TextInput
+          style={[estils.input, estils.textarea]}
+          placeholder="Comentari"
+          onChangeText={(text) => this.setState({ comentari: text })}
+          value={this.state.comentari}
+          multiline={true}
+        />
+        <Button
+          style={estils.button}
+          title="Enviar"
+          onPress={() => this.props.navigation.navigate("Home")}
+        >
+          <Text style={estils.buttonText}>Enviar</Text>
+        </Button>
+      </View>
+    );
+  }
+}
